ffc7b6ee25a162cca55e43f7031bcfcdf9785ac2
siliconvolley <rishabhlaxmikanth@gmail.com>
2023-08-26 08:58:31 +0530

    FCFS Code update

---
+++ IV Sem/Operating Systems/Program 7/P7_FCFS.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int ReadyQueue[100], i, n, TotalHeadMov=0, initial;
+	printf("Enter the number of requests: ");
+	scanf("%d", &n);
+	printf("Enter the sequence of request: ");
+	for(i=0; i<n; i++)
+		scanf("%d", &ReadyQueue[i]);
+	printf("Enter initial head position: ");
+	scanf("%d", &initial);
+	for(i=0; i<n; i++)
+	{
+		TotalHeadMov = TotalHeadMov + abs(ReadyQueue[i]-initial);
+		initial = ReadyQueue[i];
+	}
+	printf("Total Head Movement = %d",TotalHeadMov);
+}
