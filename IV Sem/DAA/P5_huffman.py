478e7aa107c83228f47316695c774eb615e2b585
siliconvolley <rishabhlaxmikanth@gmail.com>
2023-09-07 20:03:09 +0530

    update DAA program 5

--- IV Sem/DAA/P5_huffman.py
+++ IV Sem/DAA/P5_huffman.py
@@ -1,35 +1,37 @@
-import heapq 
-from collections import Counter 
- 
-class Node:     
-    def __init__(self, freq, symbol, left=None, right=None): 
-        self.freq = freq         
-        self.symbol = symbol         
-        self.left = left         
-        self.right = right 
-        self.huff = '' 
-    def __lt__(self, other): 
-        return self.freq < other.freq 
+import heapq
+from collections import Counter
 
-def printNodes(node, val=''):     
-    if not node:         
-        return     
-    newVal = val + str(node.huff)     
-    if not node.left and not node.right:         
-        print(f"{node.symbol} -> {newVal}")     
-    printNodes(node.left, newVal) 
-    printNodes(node.right, newVal) 
- 
-string = input("Enter the string: ").lower() 
-res = Counter(string) 
-print(res)
-nodes = [Node(freq, char) for char, freq in res.items()] 
-heapq.heapify(nodes) 
-while len(nodes) > 1: 
-    left = heapq.heappop(nodes)     
-    right = heapq.heappop(nodes) 
-    left.huff = '0' 
-    right.huff = '1'     
-    new_node = Node(left.freq + right.freq, left.symbol + right.symbol, left, right) 
-    heapq.heappush(nodes, new_node) 
-printNodes(nodes[0]) 
+class Node:
+    def __init__(self, freq, symbol, left=None, right=None):
+        self.freq = freq
+        self.symbol = symbol
+        self.left = left
+        self.right = right
+        self.huff = ''
+    
+    def __lt__(self, other):
+        return self.freq < other.freq
+    
+def printNodes(node, val=''):
+    if not node:
+        return
+    newVal = val + str(node.huff)
+    if not node.left and not node.right:
+        print(f"{node.symbol} -> {newVal}")
+    printNodes(node.left, newVal)
+    printNodes(node.right, newVal)
+
+string = input("Enter the string: ").lower()
+res = Counter(string)
+nodes = [Node(freq, char) for char, freq in res.items()]
+heapq.heapify(nodes)
+
+while len(nodes) > 1:
+    left = heapq.heappop(nodes)
+    right = heapq.heappop(nodes)
+    left.huff = '0'
+    right.huff = '1'
+    new_node = Node(left.freq + right.freq, left.symbol + right.symbol, left, right)
+    heapq.heappush(nodes, new_node)
+
+printNodes(nodes[0])
