e160218bb5d651f9f4ad21256a60f1c223bd4b1c
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-01-12 10:14:16 +0530

    changes in 8-puzzle.py

--- V Sem/Principles of AI/Other Programs/8-puzzle.py
+++ V Sem/Principles of AI/Other Programs/8-puzzle.py
@@ -10,20 +10,56 @@ GOAL = [
     [4, 1, 8]
 ]
 
+matching_numbers = 0
 
+def find_empty_location(board):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == '':
+                return i, j
 
 def move_up (board, empty_location):
+    i, j = find_empty_location(board)
+    if board[i+1][j]:
+        board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
+    return board
 
 def move_down(board, empty_location):
+    i, j = find_empty_location(board)
+    if board[i-1][j]:
+        board[i][j], board[i-1][j] = board[i-1][j], board[i][j]
+    return board
 
 def move_left(board, empty_location):
+    i, j = find_empty_location(board)
+    if board[i][j+1]:
+        board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
+    return board
 
-def move_right(board, empty_location:
+def move_right(board, empty_location):
+    i, j = find_empty_location(board)
+    if board[i][j-1]:
+        board[i][j], board[i][j-1] = board[i][j-1], board[i][j]
+    return board
+
+def check_matchin_numbers(board):
+    current_matching_numbers = 0
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == GOAL[i][j]:
+                current_matching_numbers += 1
+    return current_matching_numbers
+    
+
+def solve_board(board):
+    check_matchin_numbers()
+    
+
+    return
 
 def main():
     board = START
-    empty_location = [(i, row.index('')) for i, row in enumerate(GOAL) if '' in row]
-    print(board(empty_location))
+    solve_board()
 
 if __name__ == "__main__":
     main()
