a0428617bf1c67792ba7434ff066e5cf056db010
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-05-19 09:12:55 +0530

    added NLP lab programs

---
+++ VI Sem/NLP/P1.c
@@ -0,0 +1,11 @@
+// 1) Consider the following Corpus of three sentences:
+//      a) There is a big garden
+//      b) Children play in a garden
+//      c) They play inside beautiful garden
+//    Calculate P for the sentence "They play in a big garden" assuming a bigram language model
+
+#include<stdio.h>
+
+void main() {
+
+}
---
+++ VI Sem/NLP/P2_laplace_smoothing.c
@@ -0,0 +1,8 @@
+// 2) Find the bigram count for the given Corpus Apply Laplace smoothing and
+//    find the bigram probabilities after add one smoothing (up to 4 decimal places)
+
+#include<stdio.h>
+
+void main() {
+
+}
